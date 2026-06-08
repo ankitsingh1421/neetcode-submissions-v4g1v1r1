e6f9c26dc9120b4d303fd6041d99a2c6d05ac259
Ankit Singh <122378526+ankitsingh1421@users.noreply.github.com>
2026-06-08 10:35:34 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+      int n = nums.size();
+      unordered_set<int>check;
+      for(int i =0;i<n;i++){
+          if(check.count(nums[i])) return true;
+          check.insert(nums[i]);
+      }
+      return false;
+        
+    }
+};
