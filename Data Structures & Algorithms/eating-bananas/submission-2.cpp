6ec134e109d5787e7136d1ad30899d06fe15f5d4
Ankit Singh <122378526+ankitsingh1421@users.noreply.github.com>
2026-06-08 12:18:03 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int speed = 1;
+        while(true){
+            long long totaltime = 0;
+            for(int pile : piles){
+                totaltime = totaltime + ceil((double)pile/speed);
+            }
+            if(totaltime <= h){
+                return speed;
+            }
+            speed++;
+        }
+    }
+};
